5d7f1027b47f70cef44752af069b55336c4b757e
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-13 12:20:03 +0100

    NumberPrinter class moved to a separate file.

---
+++ src/main/java/com/cleancoders/primes/NumberPrinter.java
@@ -0,0 +1,35 @@
+package com.cleancoders.primes;
+
+class NumberPrinter {
+    private final int[] numbers;
+    private final int totalNumbers;
+    private final int linesPerPage;
+    private final int columns;
+
+    public NumberPrinter(int[] numbers, int totalNumbers, int linesPerPage, int columns) {
+        this.numbers = numbers;
+        this.totalNumbers = totalNumbers;
+        this.linesPerPage = linesPerPage;
+        this.columns = columns;
+    }
+
+    public void print() {
+        int pageNumber = 1;
+        int pageOffset = 1;
+        while (pageOffset <= totalNumbers) {
+            System.out.print("The First ");
+            System.out.print(totalNumbers);
+            System.out.print(" Prime Numbers --- Page ");
+            System.out.println(pageNumber);
+            for (int rowOffset = pageOffset; rowOffset < pageOffset + linesPerPage; rowOffset++) {
+                for (int column = 0; column < columns; column++)
+                    if (rowOffset + column * linesPerPage <= totalNumbers)
+                        System.out.format("%10d", numbers[rowOffset + column * linesPerPage]);
+                System.out.println();
+            }
+            System.out.println("\f");
+            pageNumber = pageNumber + 1;
+            pageOffset = pageOffset + linesPerPage * columns;
+        }
+    }
+}
