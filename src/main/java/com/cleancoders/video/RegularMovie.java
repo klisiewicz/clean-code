aa694980c7799f54d9a8959f96395d05a0df3316
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-14 11:56:59 +0100

    Redundant code removed in RegularMovie.

--- src/main/java/com/cleancoders/video/RegularMovie.java
+++ src/main/java/com/cleancoders/video/RegularMovie.java
@@ -7,29 +7,11 @@ public class RegularMovie extends Movie {
 
     @Override
     public double determineAmount(int daysRented) {
-        double rentalAmount = 0;
-        // determines the amount for each line
-        switch (priceCode) {
-            case REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
+        return (daysRented > 2) ? 2 + (daysRented - 2) * 1.5 : 2;
     }
 
     @Override
     public int determineFrequentRentalPoints(int daysRented) {
-        boolean bonusIsEarned = priceCode == NEW_RELEASE && daysRented > 1;
-        return (bonusIsEarned) ? 2 : 1;
+        return 1;
     }
 }
