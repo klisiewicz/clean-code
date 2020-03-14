8398cbfcc5d8576c8ea63381af0a62132aa33b8d
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-14 11:20:27 +0100

    determineAmount method moved from Rental to Movie.

--- src/main/java/com/cleancoders/video/Movie.java
+++ src/main/java/com/cleancoders/video/Movie.java
@@ -25,4 +25,24 @@ public class Movie {
         return title;
     }
 
+    public double determineAmount(int daysRented) {
+        double rentalAmount = 0;
+        // determines the amount for each line
+        switch (priceCode) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 }
--- src/main/java/com/cleancoders/video/Rental.java
+++ src/main/java/com/cleancoders/video/Rental.java
@@ -18,24 +18,7 @@ public class Rental {
     }
 
     public double determineAmount() {
-        double rentalAmount = 0;
-        // determines the amount for each line
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
+        return movie.determineAmount(daysRented);
     }
 
     public int determineFrequentRentalPoints() {
