d62128aa74fd3b11fa82deed9306c5c2a070c27c
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-14 11:44:15 +0100

    determineAmount and determineFrequentRentalPoints pushed down to specific Movie subclasses.

--- src/main/java/com/cleancoders/video/Movie.java
+++ src/main/java/com/cleancoders/video/Movie.java
@@ -1,12 +1,12 @@
 package com.cleancoders.video;
 
-public class Movie {
+public abstract class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     public static final int CHILDRENS = 2;
 
     private String title;
-    private int priceCode;
+    int priceCode;
 
     public Movie(String title, int priceCode) {
         this.title = title;
@@ -25,29 +25,7 @@ public class Movie {
         return title;
     }
 
-    public double determineAmount(int daysRented) {
-        double rentalAmount = 0;
-        // determines the amount for each line
-        switch (priceCode) {
-            case REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
-    }
+    public abstract double determineAmount(int daysRented);
 
-    public int determineFrequentRentalPoints(int daysRented) {
-        boolean bonusIsEarned = priceCode == NEW_RELEASE && daysRented > 1;
-        return (bonusIsEarned) ? 2 : 1;
-    }
+    public abstract int determineFrequentRentalPoints(int daysRented);
 }
--- src/main/java/com/cleancoders/video/RegularMovie.java
+++ src/main/java/com/cleancoders/video/RegularMovie.java
@@ -4,4 +4,32 @@ public class RegularMovie extends Movie {
     public RegularMovie(String title) {
         super(title, Movie.REGULAR);
     }
+
+    @Override
+    public double determineAmount(int daysRented) {
+        double rentalAmount = 0;
+        // determines the amount for each line
+        switch (priceCode) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
+
+    @Override
+    public int determineFrequentRentalPoints(int daysRented) {
+        boolean bonusIsEarned = priceCode == NEW_RELEASE && daysRented > 1;
+        return (bonusIsEarned) ? 2 : 1;
+    }
 }
