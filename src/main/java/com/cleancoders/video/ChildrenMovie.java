371fcbe9c8d51818d30cb731eebfe39e4f169210
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-14 11:55:00 +0100

    Redundant code removed in ChildrenMovie.

--- src/main/java/com/cleancoders/video/ChildrenMovie.java
+++ src/main/java/com/cleancoders/video/ChildrenMovie.java
@@ -7,29 +7,11 @@ public class ChildrenMovie extends Movie {
 
     @Override
     public double determineAmount(int daysRented) {
-        double rentalAmount = 0;
-        // determines the amount for each line
-        switch (priceCode) {
-            case REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
+        return (daysRented > 3) ? (daysRented - 3) * 1.5 : 1.5;
     }
 
     @Override
     public int determineFrequentRentalPoints(int daysRented) {
-        boolean bonusIsEarned = priceCode == NEW_RELEASE && daysRented > 1;
-        return (bonusIsEarned) ? 2 : 1;
+        return 1;
     }
 }
