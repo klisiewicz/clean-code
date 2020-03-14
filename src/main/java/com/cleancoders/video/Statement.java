cb47a62cf29eacdc1e904b7c4b3e77d507cc6081
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-14 10:53:25 +0100

    RentalLine method extracted.

--- src/main/java/com/cleancoders/video/Statement.java
+++ src/main/java/com/cleancoders/video/Statement.java
@@ -40,37 +40,41 @@ public class Statement {
 
     private String rentalLines() {
         String rentalLines = "";
-        for (Rental rental : rentals) {
-            double thisAmount = 0;
-            // determines the amount for each line
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (rental.getDaysRented() > 2)
-                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+        for (Rental rental : rentals)
+            rentalLines += rentalLine(rental);
+        return rentalLines;
+    }
 
+    private String rentalLine(Rental rental) {
+        String rentalLine = "";
+        double thisAmount = 0;
+        // determines the amount for each line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+
+        frequentRenterPoints++;
+
+        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                && rental.getDaysRented() > 1)
             frequentRenterPoints++;
 
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                    && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            rentalLines += "\t" + rental.getMovie().getTitle() + "\t"
-                    + thisAmount + "\n";
-            totalAmount += thisAmount;
-
-        }
-        return rentalLines;
+        rentalLine += "\t" + rental.getMovie().getTitle() + "\t"
+                + thisAmount + "\n";
+        totalAmount += thisAmount;
+        return rentalLine;
     }
 
     private String footer() {
