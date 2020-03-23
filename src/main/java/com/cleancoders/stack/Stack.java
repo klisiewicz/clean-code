07ea04788c163725ce727e02f81f93e1fea6d2aa
klisiewicz <karol.lisiewicz@gmail.com>
2020-03-23 10:25:42 +0100

    After one push and one pop the stack should be empty.

--- src/main/java/com/cleancoders/stack/Stack.java
+++ src/main/java/com/cleancoders/stack/Stack.java
@@ -1,14 +1,21 @@
 package com.cleancoders.stack;
 
-public class Stack {
-    private boolean isEmpty = true;
+public class Stack<T> {
     private int size = 0;
 
     public boolean isEmpty() {
-        return isEmpty;
+        return size == 0;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public void push(T element) {
+        size++;
+    }
+
+    public void pop() {
+        size--;
     }
 }
--- src/test/java/com/cleancoders/stack/StackTest.java
+++ src/test/java/com/cleancoders/stack/StackTest.java
@@ -1,15 +1,36 @@
 package com.cleancoders.stack;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StackTest {
+    private Stack<String> stack;
+
+    @Before
+    public void setUp() {
+        stack = new Stack<>();
+    }
+
     @Test
     public void newlyCreatedStackShouldBeEmpty() {
-        Stack stack = new Stack();
         assertThat(stack.isEmpty(), is(true));
         assertThat(stack.getSize(), is(0));
+    }
+
+    @Test
+    public void afterOnePushStackSizeShouldBeOne() {
+        stack.push("1");
+        assertThat(stack.isEmpty(), is(false));
+        assertThat(stack.getSize(), is(1));
+    }
+
+    @Test
+    public void afterOnePushAndOnePopStackShouldBeEmpty() {
+        stack.push("1");
+        stack.pop();
+        assertThat(stack.isEmpty(), is(true));
     }
 }
