bcd0b14a88e7b39a3c91bda6bfa9a3c6d04bc293
klisiewicz <karol.lisiewicz@gmail.com>
2020-04-03 14:48:00 +0200

    Add: GetScore method.

--- src/main/java/com/cleancoders/bowling/Game.java
+++ src/main/java/com/cleancoders/bowling/Game.java
@@ -2,6 +2,9 @@ package com.cleancoders.bowling;
 
 public class Game {
     public void roll(int pins) {
+    }
 
+    public int getScore() {
+        return 0;
     }
 }
--- src/test/java/com/cleancoders/bowling/BowlingTest.java
+++ src/test/java/com/cleancoders/bowling/BowlingTest.java
@@ -3,6 +3,9 @@ package com.cleancoders.bowling;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BowlingTest {
     private Game game;
 
@@ -14,5 +17,12 @@ public class BowlingTest {
     @Test
     public void canRoll() {
         game.roll(0);
+    }
+
+    @Test
+    public void gutterGame() {
+        for (int i = 0; i < 20; i++)
+            game.roll(0);
+        assertThat(game.getScore(), is(0));
     }
 }
