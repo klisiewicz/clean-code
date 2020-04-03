dcaca7beccda709e8bd677c4fcc49f8acc4b6fdb
klisiewicz <karol.lisiewicz@gmail.com>
2020-04-03 14:34:22 +0200

    Add: Game class.

---
+++ src/main/java/com/cleancoders/bowling/Game.java
@@ -0,0 +1,4 @@
+package com.cleancoders.bowling;
+
+public class Game {
+}
---
+++ src/test/java/com/cleancoders/bowling/BowlingTest.java
@@ -0,0 +1,10 @@
+package com.cleancoders.bowling;
+
+import org.junit.Test;
+
+public class BowlingTest {
+    @Test
+    public void canCreateGame() {
+        Game game = new Game();
+    }
+}
