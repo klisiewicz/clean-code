77fbdb1a7d5b87f21e18d5779de53f68159cad56
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-12 16:02:20 +0200

    Refactor: Extracted assertInverted method.

--- src/test/java/com/cleancoders/name/NameInverterTest.java
+++ src/test/java/com/cleancoders/name/NameInverterTest.java
@@ -8,12 +8,16 @@ import static org.hamcrest.core.Is.is;
 public class NameInverterTest {
     @Test
     public void givenNullReturnsEmptyString() {
-        assertThat(invertName(null), is(""));
+        assertInverted(null, "");
     }
 
     @Test
     public void givenEmptyStringReturnsEmptyString() {
-        assertThat(invertName(""), is(""));
+        assertInverted("", "");
+    }
+
+    private void assertInverted(String originalName, String invertedName) {
+        assertThat(invertName(originalName), is(invertedName));
     }
 
     private String invertName(String name) {
