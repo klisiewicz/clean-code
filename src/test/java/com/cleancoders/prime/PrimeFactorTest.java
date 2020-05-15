c3da9f9332d65ef241df2aa66b14e10fb0378343
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-15 13:19:54 +0200

    Refactor: While loops replaced with for.

--- src/test/java/com/cleancoders/prime/PrimeFactorTest.java
+++ src/test/java/com/cleancoders/prime/PrimeFactorTest.java
@@ -29,14 +29,9 @@ public class PrimeFactorTest {
 
     private List<Integer> primesOf(int n) {
         final ArrayList<Integer> factors = new ArrayList<>();
-        int divisor = 2;
-        while (n > 1) {
-            while (n % divisor == 0) {
+        for (int divisor = 2; n > 1; divisor++)
+            for (; n % divisor == 0; n /= divisor)
                 factors.add(divisor);
-                n /= divisor;
-            }
-            divisor++;
-        }
         return factors;
     }
 
