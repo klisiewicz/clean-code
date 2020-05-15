e184e84fec2a33bfc2740feb4f374e9b2a41ad5a
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-15 13:59:19 +0200

    WordWrapper Add: Test case for null.

---
+++ src/test/java/com/cleancoders/word/WordWrapperTest.java
@@ -0,0 +1,17 @@
+package com.cleancoders.word;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class WordWrapperTest {
+    @Test
+    public void shouldWrap() {
+        assertThat(wrap(null, 1), is(""));
+    }
+
+    private String wrap(String input, int lenght) {
+        return "";
+    }
+}
