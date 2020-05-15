0d80e68b4d3669694f95792d5eee8d7cf73685af
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-15 13:17:34 +0200

    Refactor: Divisor generalization.

--- src/test/java/com/cleancoders/prime/PrimeFactorTest.java
+++ src/test/java/com/cleancoders/prime/PrimeFactorTest.java
@@ -29,18 +29,13 @@ public class PrimeFactorTest {
 
     private List<Integer> primesOf(int n) {
         final ArrayList<Integer> factors = new ArrayList<>();
-        if (n > 1) {
-            while (n % 2 == 0) {
-                factors.add(2);
-                n /= 2;
+        int divisor = 2;
+        while (n > 1) {
+            while (n % divisor == 0) {
+                factors.add(divisor);
+                n /= divisor;
             }
-            while (n % 3 == 0) {
-                factors.add(3);
-                n /= 3;
-            }
-            if (n > 1) {
-                factors.add(n);
-            }
+            divisor++;
         }
         return factors;
     }
