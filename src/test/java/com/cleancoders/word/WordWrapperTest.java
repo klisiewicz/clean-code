1156adf7a171fa6540d588de6d3f430150d9f9a6
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-15 14:17:16 +0200

    WordWrapper Add: Acceptance test case.

--- src/test/java/com/cleancoders/word/WordWrapperTest.java
+++ src/test/java/com/cleancoders/word/WordWrapperTest.java
@@ -15,6 +15,10 @@ public class WordWrapperTest {
         assertWraps("xxx", 1, "x\nx\nx");
         assertWraps("x x", 1, "x\nx");
         assertWraps("x xx", 3, "x\nxx");
+        assertWraps("four score and seven years ago our fathers brought forth upon this continent",
+                7,
+                "four\nscore\nand\nseven\nyears\nago our\nfathers\nbrought\nforth\nupon\nthis\ncontine\nnt"
+        );
     }
 
     private void assertWraps(String input, int length, String expected) {
