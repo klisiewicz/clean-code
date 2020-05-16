155931bfdc5344178d2263966603d6f22753ba5b
klisiewicz <karol.lisiewicz@gmail.com>
2020-05-16 15:09:58 +0200

    WordWrapper Refactor: Methods extracted.

--- src/main/java/com/cleancoders/word/WordWrapper.java
+++ src/main/java/com/cleancoders/word/WordWrapper.java
@@ -3,13 +3,20 @@ package com.cleancoders.word;
 public final class WordWrapper {
     public String wrap(String input, int length) {
         if (input == null) return "";
-        if (input.length() <= length) {
-            return input;
-        } else {
-            int breakPoint = input.lastIndexOf(" ", length);
-            if (breakPoint == -1)
-                breakPoint = length;
-            return input.substring(0, breakPoint) + "\n" + wrap(input.substring(breakPoint).trim(), length);
-        }
+        return (isShorterThanLength(input, length)) ? input : wrapWithBrakingPoint(input, length);
+    }
+
+    private boolean isShorterThanLength(String input, int length) {
+        return input.length() <= length;
+    }
+
+    private String wrapWithBrakingPoint(String input, int length) {
+        int breakPoint = getBreakPoint(input, length);
+        return input.substring(0, breakPoint) + "\n" + wrap(input.substring(breakPoint).trim(), length);
+    }
+
+    private int getBreakPoint(String input, int length) {
+        int breakPoint = input.lastIndexOf(' ', length);
+        return (breakPoint >= 0) ? breakPoint : length;
     }
 }
